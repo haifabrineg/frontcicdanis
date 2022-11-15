d03d384ba2e7384bd7bb5775dfcab9a7f91d420f
haifabrineg <haifa.brineg@esprit.tn>
2022-11-15 01:36:07 +0100

    front

---
+++ src/main/java/com/esprit/examen/services/CategorieProduitServiceImpl.java
@@ -0,0 +1,52 @@
+package com.esprit.examen.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.esprit.examen.entities.CategorieProduit;
+import com.esprit.examen.repositories.CategorieProduitRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+
+public class CategorieProduitServiceImpl implements ICategorieProduitService {
+
+	
+	@Autowired
+	CategorieProduitRepository categorieProduitRepository;
+	@Override
+	public List<CategorieProduit> retrieveAllCategorieProduits() {
+		
+		return categorieProduitRepository.findAll();
+	}
+
+	@Override
+	public CategorieProduit addCategorieProduit(CategorieProduit cp) {
+		categorieProduitRepository.save(cp);
+		log.info("category added susccefully");
+
+		return cp;
+	}
+
+	@Override
+	public void deleteCategorieProduit(Long id) {
+		categorieProduitRepository.deleteById(id);
+		
+	}
+
+	@Override
+	public CategorieProduit updateCategorieProduit(CategorieProduit cp) {
+		categorieProduitRepository.save(cp);
+		log.info("category updated susccefully");
+
+		return cp;
+	}
+
+	@Override
+	public CategorieProduit retrieveCategorieProduit(Long id) {
+		return categorieProduitRepository.findById(id).orElse(null);
+	}
+
+}
---
+++ src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
@@ -0,0 +1,90 @@
+package com.esprit.examen.services;
+
+import com.esprit.examen.entities.CategorieFournisseur;
+import com.esprit.examen.entities.DetailFournisseur;
+import com.esprit.examen.entities.Fournisseur;
+import com.esprit.examen.repositories.DetailFournisseurRepository;
+import com.esprit.examen.repositories.FournisseurRepository;
+import com.esprit.examen.repositories.ProduitRepository;
+import com.esprit.examen.repositories.SecteurActiviteRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashSet;
+
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.any;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ContextConfiguration(classes = {FournisseurServiceImpl.class})
+@ExtendWith(SpringExtension.class)
+public class FournisseurServiceImplTest {
+
+    @MockBean
+    private DetailFournisseurRepository detailFournisseurRepository;
+
+    @MockBean
+    private FournisseurRepository fournisseurRepository;
+
+    @Autowired
+    private FournisseurServiceImpl fournisseurServiceImpl;
+
+    @MockBean
+    private ProduitRepository produitRepository;
+
+    @MockBean
+    private SecteurActiviteRepository secteurActiviteRepository;
+
+
+    @Test
+    void testRetrieveFournisseur() {
+
+        DetailFournisseur detailFournisseur = new DetailFournisseur();
+        detailFournisseur.setAdresse("bizerte");
+        detailFournisseur.setEmail("hassen.bm@esprit.tn");
+        detailFournisseur.setFournisseur(new Fournisseur());
+
+
+        Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setDetailFournisseur(detailFournisseur);
+        fournisseur.setIdFournisseur(1L);
+        Optional<Fournisseur> ofResult = Optional.of(fournisseur);
+        when(fournisseurRepository.findById((Long) any())).thenReturn(ofResult);
+        assertSame(fournisseur, fournisseurServiceImpl.retrieveFournisseur(1L));
+        verify(fournisseurRepository).findById((Long) any());
+    }
+
+
+
+    @Test
+    void testAddFournisseur() {
+        DetailFournisseur detailFournisseur = new DetailFournisseur();
+        detailFournisseur.setEmail("hassen.bm@esprit.tn");
+        detailFournisseur.setFournisseur(new Fournisseur());
+
+//hassen commentaire test
+        Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setCode("0000");
+        fournisseur.setDetailFournisseur(detailFournisseur);
+        assertSame(fournisseur, fournisseurServiceImpl.addFournisseur(fournisseur));
+        verify(fournisseurRepository).save((Fournisseur) any());
+    }
+
+
+}
